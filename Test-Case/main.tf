4f66765f1145dbd11fac86afa4b5d01ee8fc7561
Ubuntu <ubuntu@ip-172-31-6-248.eu-north-1.compute.internal>
2026-08-22 08:25:15 +0000

    Add test terraform

---
+++ Test-Case/main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "test" {
+  bucket = "my-checkov-test-bucket-123456"
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
